367abd442a4d028083830d2947734a17a7b85eec
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-15 16:40:55 +0300

    добавляет решение 1 задачи

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,13 @@
-// Write code under this line
-const addIndex = (element, index) => element;
-// Write code under this line
-const subIndex = (element, index) => element;
+// создает стрелочную функцию для выполнения сложения елемента массива и его индекса
+const addIndex = (element, index) => element + index;
 
-function mapArray(array, cb) {
-  // Write code under this line
-  const numbers = new Array();
+// создает стрелочную функцию для выполнения вычетания индекса от значения его елемента
+const subIndex = (element, index) => element - index;
+
+// создает функцию для перебора массива и вызова callback финкци для выполнения дейстивия вычетания или сложения
+const mapArray = function (array, cb) {
+  const numbers = new Array(array.length);
+  console.log(numbers)
   for (let i = 0; i < array.length; i += 1) {
     const element = array[i];
     const index = i;
@@ -16,8 +18,8 @@ function mapArray(array, cb) {
 
 const arr = [1, 2, 3, 4, 5];
 
-//console.log(mapArray(arr, addIndex));
+console.log(mapArray(arr, addIndex));
 // [1, 3, 5, 7, 9]
 
-//console.log(mapArray(arr, subIndex));
+console.log(mapArray(arr, subIndex));
 // [1, 1, 1, 1, 1]
