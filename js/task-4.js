6af4bb8b10eb038e528ccdb05f0880dcd6bcb94e
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-15 21:50:52 +0300

    добавляет решение 4 задачи

--- js/task-4.js
+++ js/task-4.js
@@ -3,31 +3,36 @@ const account = {
   balance: 24000,
   discount: 0.1,
   orders: ['order-1', 'order-2', 'order-3'],
+  //содаем функцию которая присваивает свойству discount значение value
   changeDiscount(value) {
-    discount = value; // Write code in this line
+    this.discount = value;
   },
+  //содаем функцию которая возвращает массив заказов
   showOrders() {
-    return orders; // Write code in this line
+    return this.orders;
   },
+  //содаем функцию которая принимает параметры стоимости и номер заказа, 
+  //отнимает от текущего баланса стоимость заказа 
+  //и добааляет номер заказа в массив заказов
   addOrder(cost, order) {
-    balance -= cost; // Write code in this line
-    orders.push(order); // Write code in this line
+    this.balance -= cost;
+    this.orders.push(order);
   },
 };
+//создаем копию обьекта делаем свойство обьекта заказы массивом
 const copyAccount =  Object.assign({},account);
 copyAccount.orders = [...account.orders];
 // копируем для автотестов ссылочные типы
 
-/*
-account.changeDiscount(0.15);
-//console.log(account.discount); // 0.15
 
-//console.log(account.showOrders()); 
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
+
+console.log(account.showOrders()); 
 // ['order-1', 'order-2', 'order-3']
 
 account.addOrder(5000, 'order-4');
-//console.log(account.balance); // 19000
+console.log(account.balance); // 19000
 
-//console.log(account.showOrders());
+console.log(account.showOrders());
 // ['order-1', 'order-2', 'order-3', 'order-4']
-*/
